de664446cdd860d0db35cb168520b4765ce2ef37
Squid_Latissimus_dorsi <icee2@naver.com>
2023-06-04 16:05:24 +0900

    LoadKeyword Controller commit
    + 키워드 목록 전송 컨트롤러

---
+++ src/main/java/yaama/LoadKeywordsController.java
@@ -0,0 +1,52 @@
+package yaama;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+/**
+ * Servlet implementation class LoadKeywordsController
+ */
+@WebServlet("/LoadKeywords")
+public class LoadKeywordsController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoadKeywordsController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/x-json; charset=UTF-8");
+		ObjectMapper om = new ObjectMapper();
+		KeywordDAO kdao = new KeywordDAO();
+		
+		List<Keyword> keywords = kdao.getAllKeyword();
+		String result = om.writeValueAsString(keywords);
+		response.getWriter().write(result);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
